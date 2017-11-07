285db4d163f4cd772a3d261c47d4c1b6fdb9f5b6
Jared Long <jarlong@csumb.edu>
2017-11-07 00:58:26 +0000

    shopping cart array working

--- shoppingcart.php
+++ shoppingcart.php
@@ -3,8 +3,24 @@ session_start();
 include '../dbConnection.php';
 $conn = getDatabaseConnection();
 
-$_SESSION['cartItems'] = array();
-$_SESSION['itemNum'] = 0;
+if(isset($_SESSION['cartItems']))
+{
+    //don't initialize "cartItems" if it already exists
+}
+else
+{
+    $_SESSION['cartItems'] = array();
+}
+
+if(isset($_SESSION['itemNum']))
+{
+    $_SESSION['itemNum'] = $_SESSION['itemNum'] + 1;
+}
+else
+{
+    $_SESSION['itemNum'] = 0;
+}
+
 function displayCart() {
     global $conn;
     $sql = "SELECT movieName 
@@ -23,15 +39,16 @@ function addToCart()
     $newItem = displayCart();
     //echo $newItem[0]['movieName'];
     //array_push($_SESSION['cartItems'],$newItem);
-    if($_SESSION['itemNum'] != 0)
-    {
+    //if($_SESSION['itemNum'] != 0)
+    //{
         $_SESSION['cartItems'][] = $newItem;
-    }
-    else
-    {
-        $_SESSION['cartItems'][$_SESSION['itemNum']] = $newItem;
-    }
-    $_SESSION['itemNum']++;
+    //}
+   // else
+    //{
+     //   $_SESSION['cartItems'][$_SESSION['itemNum']] = $newItem;
+   // }
+    
+    
     //echo $item;
     //return $_SESSION['cartItems'][0][0]['movieName'];
     //return $_SESSION[0];
@@ -41,25 +58,24 @@ function addToCart()
 <html>
     <head>
         <title>Shopping Cart</title>
-        <style>
- body{
-                width:800px;
-                margin:0 auto;
-                text-align:center;
-        }
-        </style>
+          <link href="css/main.css" rel="stylesheet" type="text/css" />
+          <link href="css/styles.css" rel="stylesheet" type="text/css" />
     </head>
     <body>
         <center>
             <h1> Shopping Cart </h1>
             <?php
               
-                addToCart();
+                if(isset ($_GET['movieId']))
+                {
+                    addToCart();
+                }
+                
                 for($i = 0;$i<count($_SESSION['cartItems']);$i++)
                 {
                     echo "Movie Title: ". $_SESSION['cartItems'][$i][0]['movieName'] . "<br>";
                 }
-                $item++;
+              
             ?>
         </center> 
         
